40f613bff1ec611e18a3792426abf81d6216bd8d
Rithvik Kasarla <rithvikrk719@gmail.com>
2018-04-23 20:03:19 -0400

    player.java

---
+++ Player.java
@@ -0,0 +1,34 @@
+package game;
+
+import java.awt.Graphics;
+
+public class Player extends GameObject
+
+{		
+
+	public Player() 
+
+	{
+
+		super(x,y,id);
+
+	}
+
+	@Override
+
+	public void tick() {
+
+		// TODO Auto-generated method stub
+
+		
+
+	}
+
+	@Override
+
+	public void render(Graphics g) {
+
+		// TODO Auto-generated method stub
+
+	}
+}
